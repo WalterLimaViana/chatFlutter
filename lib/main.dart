bec6736420299f86872de9123880f1260c7863b9
WalterLimaViana <walter.lima.viana@gmail.com>
2022-06-30 20:33:26 -0300

    dicas importantes sobre o firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,42 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async {
   runApp(MyApp());
   await Firebase.initializeApp();
+  // Subir dados no db do firebase:
+  // FirebaseFirestore.instance
+  //     .collection("mensagens")
+  //     .doc()
+  //     .set({'from': 'Carlos', 'text': 'tudo bem também'});
+  // Ler dados do db do firebase: Para ler todos os documentos utiliza-se o QuerySnapshot
+  QuerySnapshot snapshot =
+      await FirebaseFirestore.instance.collection('mensagens').get();
+  snapshot.docs.forEach((element) {
+    print(element.data());
+    element.reference.update({'lido': 'true'});
+  });
+  // Para ler somente um documento utiliza-se o DocumentSnapshot
+  DocumentSnapshot snapshotDocument = await FirebaseFirestore.instance
+      .collection('mensagens')
+      .doc('CJahXPcg70IsAln5sGGq')
+      .get();
+  print(snapshotDocument.data());
+
+  // Atualizar as mudanças de todos os documentos realizadas no DB em tempo real e ser visualizadas no app:
   FirebaseFirestore.instance
-      .collection("mensagens")
-      .doc()
-      .set({'from': 'Carlos', 'text': 'tudo bem também'});
+      .collection('mensagens')
+      .snapshots()
+      .listen((event) {
+    event.docs.forEach((element) {
+      print(element.data());
+    });
+  });
+  // Atualizar as mudanças de um único documento no DB em tempo real e ser visualizadas no app
+  FirebaseFirestore.instance
+      .collection('mensagens')
+      .doc('CJahXPcg70IsAln5sGGq')
+      .snapshots()
+      .listen((event) {
+    print(event.data());
+  });
 }
 
 class MyApp extends StatelessWidget {
